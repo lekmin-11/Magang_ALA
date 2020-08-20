4ad2be26f091b65395b3a3fe9e6a3ab1e58809dd
lekmin-11 <69658927+lekmin-11@users.noreply.github.com>
2020-08-20 20:18:33 +0700

    install wordpress vagrant
    install & konfigurasi web server dengan vagrant

---
+++ Vagrantfile
@@ -0,0 +1,29 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+
+Vagrant.configure("2") do |config|
+  
+  #config.vm.box = "base"
+  config.vm.box = "ubuntu/bionic64"
+  config.vm.box_version = "20200812.0.0"
+
+  # config.vm.box_check_update = false
+
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
+  config.vm.network "private_network", ip: "192.168.33.15"
+  # config.vm.network "public_network"
+  # config.vm.synced_folder "../data", "/vagrant_data"
+
+  config.vm.provider "virtualbox" do |vb|
+  #   # Display the VirtualBox GUI when booting the machine
+  #   vb.gui = true
+  #
+  #   # Customize the amount of memory on the VM:
+     vb.memory = "1024"
+  end
+  config.vm.provision "shell", path: "install.sh"
+  config.vm.provision "shell", path: "konfigurasi.sh"
+  # SHELL
+end
---
+++ konfigurasi.sh
@@ -0,0 +1,30 @@
+echo "masuk direktory home"
+cd /home
+echo "download wordpress"
+sudo wget https://wordpress.org/latest.tar.gz
+echo "exstrak file wordpress"
+sudo tar xf latest.tar.gz
+echo "copy file wordpress"
+sudo cp -r wordpress/ /var/www/html/
+cd
+sudo chmod -R 777 /var/www/html/wordpress/
+
+DBHOST=localhost
+DBNAME=wordpress
+DBUSER=ricky
+DBPASSWD=test123
+echo "add database"
+sudo mysql -uroot -e "CREATE DATABASE $DBNAME"
+echo "add user to databases"
+sudo mysql -uroot -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
+
+echo "copy file web.conf"
+sudo cp /vagrant/config/web.conf /etc/apache2/sites-available/
+echo "enable web.conf"
+sudo a2ensite web.conf
+sudo systemctl restart apache2
+echo "disabel 00-default"
+sudo a2dissite 000-default.conf
+echo "restart apache2"
+sudo systemctl restart apache2
+echo "buka browser dengan IP :192.168.33.15"
